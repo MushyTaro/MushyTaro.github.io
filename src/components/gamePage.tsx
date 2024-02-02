304fc87a569249aeaf355804b34b96b9147de3a7
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-02 22:29:38 +0800

    feat: added gameboard component

--- src/components/gamePage.tsx
+++ src/components/gamePage.tsx
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
+import Board from "./gameBoard";
+import "../styles/gamePage.css";
 function GamePage() {
-  const { difficulty, discColor } = useParams();
-  return (
-    <div>
-      <h2>Game Page</h2>
-      <p>Difficulty: {difficulty}</p>
-      <p>Disc Color: {discColor}</p>
-    </div>
-  );
+	const { difficulty, discColor } = useParams();
+	return (
+		<div className="game_page_container">
+			<h2>Game Page</h2>
+			<p>Difficulty: {difficulty}</p>
+			<p>Disc Color: {discColor}</p>
+			<Board />
+		</div>
+	);
 }
 
 export default GamePage;
