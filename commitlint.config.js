173ea562b49f9cd913c38dbcfc9d42b3f941189a
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-02 16:30:08 +0800

    build: update the project environment

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "scope-case": [2, "always", ["camel-case", "pascal-case"]],
-    "subject-case": [2, "always", ["lower-case","sentence-case", "start-case", "pascal-case", "upper-case"]],
-  },
-};
