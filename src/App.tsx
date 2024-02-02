851867e7d20112811b74f3c733350a7969822958
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-02 16:57:52 +0800

    feat: added the mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import MainPage from "./components/mainPage.tsx";
 function App() {
-	return <></>;
+	return <MainPage />;
 }
 
 ReactDOM.createRoot(document.getElementById("content")!).render(
---
+++ src/components/mainPage.tsx
@@ -0,0 +1,66 @@
+import white_disc_imagePath from "../assets/white-disc.png";
+import black_disc_imagePath from "../assets/black-disc.png";
+import { useState } from "react";
+
+function MainPage() {
+	const [selectedDifficulty, setSelectedDifficulty] = useState("Easy");
+	const [selectedDiscColor, setSelectedDiscColor] = useState("White");
+
+	const handleDifficulty = (difficulty: "Easy" | "Hard") => {
+		setSelectedDifficulty(difficulty);
+	};
+	const handleDiscColor = (discColor: "Black" | "White") => {
+		setSelectedDiscColor(discColor);
+	};
+
+	return (
+		<>
+			<div className="main_page_container">
+				<h1 className="game_title">Reversi</h1>
+				<div className="difficulty">
+					Difficulty:
+					<button
+						className={`difficultyButton ${
+							selectedDifficulty === "Easy" ? "selected" : ""
+						}`}
+						onClick={() => handleDifficulty("Easy")}>
+						Easy
+					</button>
+					<button
+						className={`difficultyButton ${
+							selectedDifficulty === "Hard" ? "selected" : ""
+						}`}
+						onClick={() => handleDifficulty("Hard")}>
+						Hard
+					</button>
+				</div>
+				<div className="select_disc_color">
+					Play As:
+					<div
+						className={`discColorButtonWrapper ${
+							selectedDiscColor === "White" ? "selected" : ""
+						}`}
+						onClick={() => handleDiscColor("White")}>
+						<img
+							className="discColorButton"
+							src={white_disc_imagePath}
+							alt="White Disc"></img>
+					</div>
+					<div
+						className={`discColorButtonWrapper ${
+							selectedDiscColor === "Black" ? "selected" : ""
+						}`}
+						onClick={() => handleDiscColor("Black")}>
+						<img
+							className="discColorButton"
+							src={black_disc_imagePath}
+							alt="Black Disc"></img>
+					</div>
+				</div>
+				<button className="submitButton">Submit</button>
+			</div>
+		</>
+	);
+}
+
+export default MainPage;
