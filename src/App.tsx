63d68a17987b07e84ca211ff4b367176df44836f
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-02 17:53:03 +0800

    feat: added the routing from the homepage to the gamepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MainPage from "./components/mainPage.tsx";
+import GamePage from "./components/gamePage.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
-function App() {
-	return <MainPage />;
-}
+
+const App = () => (
+	<Router>
+		<Routes>
+			<Route path="/" element={<MainPage />} />
+			<Route path="/game/:difficulty/:discColor" element={<GamePage />} />
+		</Routes>
+	</Router>
+);
 
 ReactDOM.createRoot(document.getElementById("content")!).render(
 	<React.StrictMode>
