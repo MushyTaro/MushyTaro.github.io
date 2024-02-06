5ed63142b7fc7595c16e6625915499473c8a0695
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-06 17:53:51 +0800

    test: test for commit error

--- src/components/gameBoard.tsx
+++ src/components/gameBoard.tsx
@@ -1,54 +1,51 @@
-import white_disc_imagePath from "../assets/white-disc.png";
 import black_disc_imagePath from "../assets/black-disc.png";
+import white_disc_imagePath from "../assets/white-disc.png";
 
 interface SquareProps {
-	value: Disc;
+  value: Disc;
 }
 
 type Disc = "B" | "W" | "";
 
 function Square({ value }: SquareProps) {
-	if (value == "B") {
-		return (
-			<div className="square">
-				<img className="disc" src={black_disc_imagePath} alt="Black Disc" />
-			</div>
-		);
-	}
-	if (value == "W") {
-		return (
-			<div className="square">
-				<img className="disc" src={white_disc_imagePath} alt="White Disc" />
-			</div>
-		);
-	}
-	if (value == "") {
-		return <div className="square"></div>;
-	}
+  if (value === "B") {
+    return (
+      <div className="square">
+        <img className="disc" src={black_disc_imagePath} alt="Black Disc" />
+      </div>
+    );
+  }
+  if (value === "W") {
+    return (
+      <div className="square">
+        <img className="disc" src={white_disc_imagePath} alt="White Disc" />
+      </div>
+    );
+  }
+  if (value === "") {
+    return <div className="square" />;
+  }
 }
 export default function Board() {
-	// Function to render a single row of the board
-	const board: Disc[][] = [
-		["", "", "", "", "", "", "", ""],
-		["", "", "", "", "", "", "", ""],
-		["", "", "", "", "", "", "", ""],
-		["", "", "", "W", "B", "", "", ""],
-		["", "", "", "B", "W", "", "", ""],
-		["", "", "", "", "", "", "", ""],
-		["", "", "", "", "", "", "", ""],
-		["", "", "", "", "", "", "", ""],
-	];
-	return (
-		<>
-			{
-				<div className="reversi-board">
-					{board.map((row, rowIndex) =>
-						row.map((value, colIndex) => (
-							<Square key={`${rowIndex}-${colIndex}`} value={value} />
-						))
-					)}
-				</div>
-			}
-		</>
-	);
+  // Function to render a single row of the board
+  const board: Disc[][] = [
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "W", "", "", "", ""],
+    ["", "", "", "W", "B", "", "", ""],
+    ["", "", "", "B", "W", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+  ];
+  return (
+    <div className="reversi-board">
+          {board.map((row, rowIndex) =>
+            row.map((value, colIndex) => {
+              const key = `${rowIndex}-${colIndex}`;
+              return <Square key={key} value={value} />;
+            })
+          )}
+        </div>
+  );
 }
