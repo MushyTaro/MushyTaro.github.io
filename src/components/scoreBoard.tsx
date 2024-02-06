7766f54babd955d7165b13cd94baf8473aa39b85
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-06 10:31:40 +0800

    style: Rearraging classNames for better readability

--- src/components/scoreBoard.tsx
+++ src/components/scoreBoard.tsx
@@ -14,17 +14,19 @@ export default function ScoreBoard({ difficulty, discColor }: ScoreBoardProps) {
 	return (
 		<>
 			<div className="scoreBoard">
-				<div className="playerCard">Player</div>
-				<div className={`playerDiscCard ${playerDiscImage}`}>
+				<div className="playerCard playerNameCard">Player</div>
+				<div className={`playerCard playerDiscCard ${playerDiscImage}`}>
 					<img src={playerDiscImagePath} alt="Disk"></img>
 					<div className="centered">1</div>
 				</div>
 				<span className="scoreBoardText">Scores</span>
-				<div className={`computerDiscCard ${computerDiscImage}`}>
+				<div className={`computerCard computerDiscCard ${computerDiscImage}`}>
 					<img src={computerDiscImagePath} alt="Disk" />
 					<div className="centered">1</div>
 				</div>
-				<div className="computerCard">Computer:{difficulty}</div>
+				<div className="computerCard computerNameCard">
+					Computer:{difficulty}
+				</div>
 			</div>
 		</>
 	);
