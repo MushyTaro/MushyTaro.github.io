c63f0d00f0c1ed035f7bc877f9acf84695a907ea
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-08 10:43:50 +0800

    refactor: restructuring the gameboard component

--- src/components/game-page/GameBoard.tsx
+++ src/components/game-page/GameBoard.tsx
@@ -5,36 +5,25 @@ import { DiscColor } from "../../types";
 type GridValue = DiscColor | "";
 
 function Grid({ value }: { value: GridValue }) {
-  if (value === "B") {
-    return (
-      <div className="gameboard-grid">
-        <img className="gameboard-grid__disc" src={black_disc_imagePath} alt="Black Disc" />
-      </div>
-    );
-  }
-  if (value === "W") {
-    return (
-      <div className="gameboard-grid">
-        <img className="gameboard-grid__disc" src={white_disc_imagePath} alt="White Disc" />
-      </div>
-    );
-  }
-  if (value === "") {
-    return <div className="gameboard-grid" />;
-  }
+  return value === "" ? (
+    <div className="gameboard-grid" />
+  ) : (
+    <div className="gameboard-grid">
+      <img
+        className="gameboard-grid__disc"
+        src={value === "W" ? white_disc_imagePath : black_disc_imagePath}
+        alt={`${value === "W" ? "White" : "Black"} Disc`}
+      />
+    </div>
+  );
 }
 export default function GameBoard() {
   // Function to render a single row of the board
-  const initialBoard: GridValue[][] = [
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "W", "B", "", "", ""],
-    ["", "", "", "B", "W", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-  ];
+  const initialBoard: GridValue[][] = Array.from({ length: 8 }, () => Array(8).fill(""));
+  initialBoard[3][3] = "W";
+  initialBoard[4][4] = "W";
+  initialBoard[3][4] = "B";
+  initialBoard[4][3] = "B";
   return (
     <div className="reversi-board">
       {initialBoard.map((row, rowIndex) =>
