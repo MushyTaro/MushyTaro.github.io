677988f3ac805910c1a45cfbfbb8ed99cc11bdb4
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-02-15 15:51:28 +0800

    feat: add popup message

--- src/components/game-page/GamePage.tsx
+++ src/components/game-page/GamePage.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { HandleTurn } from "../../logic/HandleTurn";
 import { markValidMoves } from "../../logic/ValidLogic";
 import { Difficulty, DiscColor, GridValue } from "../../types";
+import Popup from "../Popup";
 import Board from "./GameBoard";
 import "../../styles/game-page/GamePage.css";
 import ScoreBoard from "./ScoreBoard";
@@ -16,13 +17,18 @@ function GamePage() {
   const initialBoard: GridValue[][] = Array.from({ length: 8 }, () => Array(8).fill(""));
   const centerRow = Math.floor(initialBoard.length / 2);
   const centerCol = Math.floor(initialBoard[0].length / 2);
-
   initialBoard[centerRow - 1][centerCol - 1] = "W";
   initialBoard[centerRow][centerCol] = "W";
   initialBoard[centerRow - 1][centerCol] = "B";
   initialBoard[centerRow][centerCol - 1] = "B";
+
   const [board, setBoard] = useState<GridValue[][]>(markValidMoves(discColor, initialBoard));
   const [currentPlayer, setCurrentPlayer] = useState<DiscColor>("B");
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [popupMessage, setPopupMessage] = useState<string>("");
+  const closePopup = () => {
+    setPopupVisible(false);
+  };
 
   const updateGame = (updatedBoard: GridValue[][]) => {
     const { nextUpdatedBoard, nextPlayer, message } = HandleTurn({ updatedBoard, currentPlayer, discColor });
@@ -30,24 +36,24 @@ function GamePage() {
       const skipPlayer = currentPlayer === "B" ? "W" : "B";
       setBoard(markValidMoves(skipPlayer, updatedBoard));
       const skipMessage = currentPlayer === discColor ? "Computer has been skipped" : "Player has been skipped";
-      setTimeout(() => {
-        window.alert(skipMessage);
-        setBoard(nextUpdatedBoard);
-      }, 0);
+      setPopupMessage(skipMessage);
+      setPopupVisible(true);
+      setBoard(nextUpdatedBoard);
     } else if (message === "end") {
       setBoard(nextUpdatedBoard);
-      setTimeout(() => {
-        window.alert("The game has ended");
-      }, 0);
+      setPopupMessage("The game has ended");
+      setPopupVisible(true);
     } else {
       setCurrentPlayer(nextPlayer);
       setBoard(nextUpdatedBoard);
     }
   };
+
   return (
     <div className="game-page-container">
       <Board board={board} playerTurn={currentPlayer} onBoardPlay={updateGame} />
       <ScoreBoard difficulty={difficulty} discColor={discColor} currentPlayer={currentPlayer} />
+      <Popup show={popupVisible} onClose={closePopup} messageContent={popupMessage} />
     </div>
   );
 }
