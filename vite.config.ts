691a93f33e6ab992db248de6995fa4fd1de17d5a
Soon Chen Yew <96568768+MushyTaro@users.noreply.github.com>
2024-03-07 16:01:09 +0800

    build: changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/reversi-scy/",
   plugins: [react()],
+  server: {
+    host: true,
+  },
 });
